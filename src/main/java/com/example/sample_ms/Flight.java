fc77b119c7875c1dedabf2b9855f9c950abf67df
Payal-282000 <pbad2809@gmail.com>
2025-03-15 13:58:27 +0530

    adding new java class flight

---
+++ src/main/java/com/example/sample_ms/Flight.java
@@ -0,0 +1,10 @@
+package com.example.sample_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Flight {
+    @GetMapping("/Flight")
+    public String getData() {return "Please book your Flight ticket for Mumbai in 50% discount" ; }
+}
