67483d09ab52aa4c58e4f7d7c6ed7c0d3328e9bc
Payal-282000 <payalbadgujar200028@gmail.com>
2025-03-16 09:45:27 +0530

    Update Flight.java

--- src/main/java/com/example/sample_ms/Flight.java
+++ src/main/java/com/example/sample_ms/Flight.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Flight {
     @GetMapping("/Flight")
-    public String getData() {return "Please book your Flight ticket for Mumbai in 50% discount" ; }
+    public String getData() {return "Please book your Flight from Indigo kindly book ticket for Mumbai in 50% discount" ; }
 }
 
